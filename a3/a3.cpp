ce27fa68c3b993a1258ddd9b5fd4522f6709ace2
Nestor Tamares <jrtamares@gmail.com>
2016-06-21 22:39:48 -0700

    Added alarm_clock
    So far, only displays current time

--- a3/a3.cpp
+++ a3/a3.cpp
@@ -1,3 +1,17 @@
+// Nestor Tamares (masc2010) & Alvin Somvilay (masc2006)
+// CS 570, Summer 2016
+// Guy Leonard
+// Assignment #3, Alarm Clock
+// a3.cpp
+
+#include "alarm_clock.h"
+
 int main () {
+    
+    while(1){
+        display_time();
+	sleep(1);
+    }
+    
     return 0;
 }
---
+++ a3/alarm_clock.h
@@ -0,0 +1,12 @@
+#ifndef ALARM_CLOCK_H
+#define ALARM_CLOCK_H
+
+#include <iostream>
+#include <ctime>
+
+using namespace std; 
+
+// Displays current time
+void display_time();
+
+#endif // ALARM_CLOCK_H
